f297d9fd01fa13ed8075fc441f2e4fedc13be241
pelu97 <kalangoandrade@yahoo.com.br>
2019-05-07 15:34:45 -0300

    adicionado comentario com o prototipo do algoritmo das passagens

--- trab1/assembler/passage1/passage1.cpp
+++ trab1/assembler/passage1/passage1.cpp
@@ -7,17 +7,33 @@ FileReader* Passage1::getFileReader(string filename){
 
 //override
 void Passage1::run(FileReader *rawFile){
-  
+
     bool eof = false;
     while(!eof){
 
-        string line = rawFile->readNextLine();
-        if (line == "-1")
-            eof = true;
+        string line = rawFile->readNextLine();      //recebe uma linha nova do arquivo
 
+        if (line == "-1"){          //checa se o arquivo acabou
+            eof = true;
+        }
         else{
             cout << line << endl;
         }
-    }    
-    
+
+        //separa linha em rotulo, operacao e operando
+
+        //se existir rotulo
+            //procura rotulo na tabela de simbolos
+                //achou - erro, simbolo redefinido
+                //nao achou - insere rotulo e contador de posicao na tabela
+
+        //procura operacao na tabela de instrucoes
+            //achou - contador de posicao = contador de posicao + tamanho da instrucao
+            //nao achou
+                //procura operacao na tabela de diretivas
+                //achou - executa diretiva / contador de posicao = valor retornado pela subrotina da diretiva
+                //nao achou - erro, operacao nao identificada
+            //contador de linha++
+    }
+
 }
--- trab1/assembler/passage2/passage2.cpp
+++ trab1/assembler/passage2/passage2.cpp
@@ -9,21 +9,33 @@ FileReader* Passage2::getFileReader(string filename){
 FileWriter* Passage2::getFileWriter(string filename){
     return new FileWriter(filename);
 }
-    
+
 //override
 void Passage2::run(FileReader *rawFile, FileWriter *preprocFile){
-  
+
     bool eof = false;
     while(!eof){
 
-        string line = rawFile->readNextLine();
-        if (line == "-1")
+        string line = rawFile->readNextLine();      //recebe uma nova linha do arquivo
+        if (line == "-1"){      //checa se o arquivo acabou
             eof = true;
-
+        }
         else{
             cout << line << endl;
-            preprocFile->writeNextLine(line);
+            preprocFile->writeNextLine(line);       //grava linha no arquivo objeto
         }
-    }    
-    
+
+        //separa os elementos da linha, ignora rotulos
+        //para cada operando que for um simbolo, procura na tabela de simbolos
+            //nao achou - erro, simbolo indefinido
+            //achou - contador de posicao = contador de posicao + tamanho da instrucao
+                //se numero e tipo dos operandos esta correto
+                    //gera codigo objeto conforme formato da instrucao
+                //senao - erro, operacao nao identificada
+            //nao achou - procura operacao na tabela de diretivas
+                //achou - executa diretiva / contador de posicao = valor retornado pela subrotina da diretiva
+                //nao achou - erro, operacao nao identificada
+        //contador de linha++
+    }
+
 }

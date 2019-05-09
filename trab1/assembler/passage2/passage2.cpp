6b3d50ac5f42c91ca33491c0353d6107aa50835f
pelu97 <kalangoandrade@yahoo.com.br>
2019-05-09 14:59:52 -0300

    pseudocodigo do algoritmo das passagens 1 e 2

--- trab1/assembler/passage2/passage2.cpp
+++ trab1/assembler/passage2/passage2.cpp
@@ -9,10 +9,10 @@ FileReader* Passage2::getFileReader(string filename){
 FileWriter* Passage2::getFileWriter(string filename){
     return new FileWriter(filename);
 }
-    
+
 //override
 void Passage2::run(FileReader *rawFile, FileWriter *preprocFile){
-  
+
     bool eof = false;
     while(!eof){
 
@@ -24,6 +24,18 @@ void Passage2::run(FileReader *rawFile, FileWriter *preprocFile){
             cout << line << endl;
             preprocFile->writeNextLine(line);
         }
-    }    
-    
+
+        //separa os elementos da linha, ignora rotulos
+        //para cada operando que for um simbolo, procura na tabela de simbolos
+            //nao achou - erro, simbolo indefinido
+            //achou - contador de posicao = contador de posicao + tamanho da instrucao
+                //se numero e tipo dos operandos esta correto
+                    //gera codigo objeto conforme formato da instrucao
+                //senao - erro, operacao nao identificada
+            //nao achou - procura operacao na tabela de diretivas
+                //achou - executa diretiva / contador de posicao = valor retornado pela subrotina da diretiva
+                //nao achou - erro, operacao nao identificada
+        //contador de linha++
+    }
+
 }

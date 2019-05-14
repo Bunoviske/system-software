61d32746fc512d839337ae55126a0884305e8cbf
Bruno Fernandes Carvalho <brunofcarvalho1996@gmail.com>
2019-05-13 22:40:24 -0300

    funcao de pegar tokens excluindo comentarios, \t e \n foi retirado da classe e colocada em modulo a parte, pois tb é usada pelo processamento de macros

---
+++ trab1/assembler/preproc/preprocParsingFunction.hpp
@@ -0,0 +1,10 @@
+#ifndef PARSING_PREPROC
+#define PARSING_PREPROC
+
+#include "../interfaces/preprocInterface.hpp"
+#include "../../utils/tablesService.hpp"
+#include "../errorAnalysis/errorService.hpp"
+
+vector<string> getTokensOfLine(string line);
+
+#endif

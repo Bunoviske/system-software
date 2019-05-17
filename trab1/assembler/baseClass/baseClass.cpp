407fc4449ff0d1d497f85fbe51822f8ab93c7d2c
Bruno Fernandes Carvalho <brunofcarvalho1996@gmail.com>
2019-05-17 15:38:52 -0300

    insercao da fucnao getTokens na baseClass

--- trab1/assembler/baseClass/baseClass.cpp
+++ trab1/assembler/baseClass/baseClass.cpp
@@ -12,3 +12,31 @@ std::string BaseClass::toUpperCase(std::string s){
 
     return temp;
 }
+
+vector<string> BaseClass::getTokensOfLine(string line)
+{
+    vector<string> tokens;
+    string currentWord = "";
+
+    for (size_t i = 0; i < line.size(); i++)
+    {
+        char charac = line[i];
+        if (charac != ' ' && charac != '\n' && charac != '\t')
+        {
+            if (charac == ';')
+                //se for comentário, acaba a linha
+                break;
+            else
+                currentWord += charac; //se for um caractere valido, adiciona ao token que esta sendo formado
+        }
+        else if (currentWord != "")
+        {
+            tokens.push_back(currentWord);
+            currentWord = "";
+        }
+    }
+    if (currentWord != "")
+        tokens.push_back(currentWord);
+
+    return tokens;
+}
--- trab1/assembler/baseClass/baseClass.hpp
+++ trab1/assembler/baseClass/baseClass.hpp
@@ -1,19 +1,25 @@
 #ifndef BASECLASS
 #define BASECLASS
 
+#include <iostream>
 #include <string>
 #include <locale>
+#include <vector>
 
-// using namespace std;
+using namespace std;
 
-
-class BaseClass{
+//basePassage é usado pelo preprocessamento, processamento de macro, passagem 1 e 2
+class BaseClass{ 
 
 public:
     BaseClass() { }; //construtor
     ~BaseClass() { }; //destrutor
 
+protected:
+
     std::string toUpperCase(std::string s);
+    vector<string> getTokensOfLine(string line);
+
 };
 
 

6aaffb113c41c53f8d016c9b5246c94b85dfa270
Bruno Fernandes Carvalho <brunofcarvalho1996@gmail.com>
2019-07-01 15:59:01 -0300

    geracao de arquivo executavel funcionando para o caso mais simples, com apenas instrucoes MOV

--- trab2/ligador_ia32/linker.cpp
+++ trab2/ligador_ia32/linker.cpp
@@ -11,56 +11,23 @@ void Linker::run(string &text, string &data, string filename)
 
 void Linker::writeTextSection(string &text, string &data, vector<string> tokens)
 {
-
-    if (tokens[0] == "GLOBAL" || tokens[0] == "_start:") //pula quando eh a sinalizacao de start
+    // cout << "Write Text Section" << endl;
+    if (tokens[0] == "GLOBAL" || tokens[0] == "_start:") //pula quando eh a sinalizacao de _start
         return;
 
-    for (size_t i = 0; i < tokens.size(); i++)
-    {
-        if (isInstructionInTable(tokens[i])) //text += opcode
-        {
-        }
-        else if (isSymbolInTable(tokens[i])) //text += address do simbolo (displacement ou offset)
-        {
-            //cout << tokens[i] << endl;
-            uint32_t val = DATA_SEGMENT_ADDRESS + symbolsTable[tokens[i]]; //numero ja tem os 32 bits extendidos pois DATA SEGMENT tem 32bits
-            text += hex2ascii(toLittleEndian(val), false);
-            // cout << hex << val << endl;
-        }
-        else if (tokens[i][0] == '[') //tem que acessar o dado armazenado
-        {
-            //retira [ ]
-            tokens[i].pop_back();
-            tokens[i].erase(tokens[i].begin());
-            if (isSymbolInTable(tokens[i]))
-            {
-                int index = symbolsTable[tokens[i]]; //acessa secao de dados e pega valor da memoria
-                //cout << data.substr(index, 8) << endl;
-                text += data.substr(index, 8); //8 digitos em hexadecimal representam 32 bits
-            }
-            else if (tokens[i].find('+') != string::npos)
-            {
-                // cout << tokens[i] << endl;
-                // cout << getAddressDisplacement(tokens[i]) << endl;
-                // cout << getLabelName(tokens[i]) << endl;
-            }
-            else 
-                cout << "Erro na montagem das instrucoes" << endl;
-        }
-        else if (tokens[i].find('+') != string::npos) //displacement do endereco de memoria
-        {
-            // cout << tokens[i] << endl;
-            // cout << getAddressDisplacement(tokens[i]) << endl;
-            // cout << getLabelName(tokens[i]) << endl;
-        }
-        else
-        {
-            cout << "Erro na montagem das instrucoes" << endl;
-        }
-    }
+    if (isInstructionInTable(tokens[0])) //text += opcode
+        text += getInstructionOpcode(tokens, nullptr);
+    else
+        cout << "Instrucao inexistente" << endl;
+
+    if (tokens[0] == "INT")
+        return; //argumento da instrucao INT nao deve ser processado, pois seu numero ja esta incluido no opcode
+
+    getArgumentsBinaryCode(text, data, tokens, isJumpInstruction(tokens[0]));
 
     // text = hex2ascii(0xB804000000) + hex2ascii(0xBB01000000) + hex2ascii(0xB900000011) +
     //        hex2ascii(0xBA0E000000) + hex2ascii(0xCD80) + hex2ascii(0xB801000000) + hex2ascii(0xCD80);
+    cout << text << endl;
 }
 void Linker::writeDataSection(string &data, vector<string> tokens)
 {
@@ -91,11 +58,18 @@ void Linker::firstPassage(string &data, string filename)
         if (line == "-1")
             eof = true;
         else
-            searchSymbol(data, getTokensOfLine(line));
+        {
+            vector<string> tokens = getTokensOfLine(line);
+            if (tokens.size() > 0)
+                searchSymbol(data, tokens);
+        }
     }
-
-    // for (std::map<string, uint32_t>::iterator it = symbolsTable.begin(); it != symbolsTable.end(); ++it)
-    //     std::cout << it->first << " => " << it->second << '\n';
+    cout << "Tabela de simbolos na secao de texto:" << endl;
+    for (std::map<string, uint32_t>::iterator it = textSymbolsTable.begin(); it != textSymbolsTable.end(); ++it)
+        std::cout << it->first << " => " << it->second << '\n';
+    cout << "Tabela de simbolos na secao de dados:" << endl;
+    for (std::map<string, uint32_t>::iterator it = dataSymbolsTable.begin(); it != dataSymbolsTable.end(); ++it)
+        std::cout << it->first << " => " << it->second << '\n';
 
     closeFile(myFile);
 }
@@ -113,10 +87,111 @@ void Linker::secondPassage(string &text, string &data, string filename)
             eof = true;
         else
         {
-            writeBinaryInstructions(text, data, getTokensOfLine(line));
+            vector<string> tokens = getTokensOfLine(line);
+            if (tokens.size() > 0)
+                writeBinaryInstructions(text, data, tokens);
         }
     }
     closeFile(myFile);
+}
+void Linker::getArgumentsBinaryCode(string &text, string &data, vector<string> tokens, bool isJump)
+{
+    for (size_t i = 1; i < tokens.size(); i++)
+    {
+        if (isSymbolInTable(tokens[i])) //text += address do simbolo (displacement ou offset)
+        {
+            //cout << tokens[i] << endl;
+            uint32_t val = DATA_SEGMENT_ADDRESS + (isJump ? textSymbolsTable[tokens[i]] : dataSymbolsTable[tokens[i]]);
+            text += hex2ascii(toLittleEndian(val), true);
+            // cout << hex << val << endl;
+        }
+        else if (tokens[i][0] == '[') //tem que acessar o dado armazenado
+        {
+            //retira [ ]
+            tokens[i].pop_back();
+            tokens[i].erase(tokens[i].begin());
+            if (isSymbolInTable(tokens[i]))
+            {
+                int index = dataSymbolsTable[tokens[i]]; //acessa secao de dados e pega valor da memoria
+                //cout << data.substr(index, 8) << endl;
+                text += data.substr(index, 8); //8 digitos em hexadecimal representam 32 bits
+            }
+            else if (tokens[i].find('+') != string::npos)
+            {
+                string label = getLabelName(tokens[i]);
+                if (!isSymbolInTable(label))
+                    cout << "Simbolo nao definido" << endl;
+
+                int displacement = getAddressDisplacement(tokens[i]);
+                int index = dataSymbolsTable[label] + displacement; //acessa secao de dados e pega valor da memoria
+                text += data.substr(index, 8);                      //8 digitos em hexadecimal representam 32 bits
+                // cout << getAddressDisplacement(tokens[i]) << endl;
+                // cout << getLabelName(tokens[i]) << endl;
+                cout << hex << data.substr(index, 8) << ' ' << tokens[i] << endl;
+            }
+            else
+                cout << "Registrador dentro de []" << endl;
+        }
+        else if (tokens[i].find('+') != string::npos) //displacement do endereco de memoria
+        {
+
+            string label = getLabelName(tokens[i]);
+            if (!isSymbolInTable(label))
+                cout << "Simbolo nao definido" << endl;
+
+            int displacement = getAddressDisplacement(tokens[i]);
+            uint32_t val = DATA_SEGMENT_ADDRESS + dataSymbolsTable[label] + displacement;
+            text += hex2ascii(toLittleEndian(val), true);
+            // cout << hex << val << ' ' << tokens[i] << endl;
+        }
+        else if (isDecimalNumber(tokens[i])) //imediato
+        {
+            uint32_t val = stoi(tokens[i]);
+            text += hex2ascii(toLittleEndian(val), true);
+        }
+        else if (isHexadecimalNumber(tokens[i])) //imediato
+        {
+            uint32_t val = stoi(tokens[i], nullptr, 16);
+            text += hex2ascii(toLittleEndian(val), true);
+        }
+    }
+}
+string Linker::getInstructionOpcode(vector<string> tokens, int *nextTokenToProcess)
+{
+    string completeInstruction = tokens[0];
+    for (size_t i = 1; i < tokens.size(); i++)
+    {
+        if (tokens[i] == "DWORD" || tokens[i] == "WORD" || tokens[i] == "BYTE")
+            continue;
+
+        //se nao tiver na tabela e nao for imediato, adiciona a instrucao completa para verificar opcode.
+        //TODO - Nao suporta displacement e nem acesso com [] !
+        if (isSymbolInTable(tokens[i]) || isDecimalNumber(tokens[i]) || isHexadecimalNumber(tokens[i]))
+        {
+            if (tokens[i] == "0X80" && tokens[0] == "INT")
+                //caso especial em que INT 0X80 ja é totalmente representado no opcode
+                completeInstruction += " " + tokens[i];
+            continue;
+        }
+
+        completeInstruction += " " + tokens[i];
+
+        // if (tokens[i][0] == '[')
+        // {
+        //     //retira []
+        //     tokens[i].pop_back();
+        //     tokens[i].erase(tokens[i].begin());
+        //     if (tokens[i].find('+') != string::npos)
+        //     {
+        //         string operand = getLabelName(tokens[i]);
+        //         completeInstruction += isSymbolInTable(operand) ? "" : operand;
+        //     }
+        //     else
+        //         completeInstruction += isSymbolInTable(tokens[i]) ? "" : tokens[i];
+        // }
+    }
+    cout << completeInstruction << endl;
+    return opcodesTable[completeInstruction];
 }
 
 void Linker::writeBinaryInstructions(string &text, string &data, vector<string> tokens)
@@ -139,12 +214,12 @@ void Linker::writeBinaryInstructions(string &text, string &data, vector<string> 
 
 bool Linker::setCurrentSection(vector<string> tokens)
 {
-    if (tokens[0] == "SECTION" && tokens[1] == ".TEXT")
+    if (tokens[0] == "SECTION" && tokens[1] == ".text")
     {
         currentSection = TEXT_SECTION;
         return true;
     }
-    else if (tokens[0] == "SECTION" && tokens[1] == ".DATA")
+    else if (tokens[0] == "SECTION" && tokens[1] == ".data")
     {
         currentSection = DATA_SECTION;
         return true;
@@ -167,6 +242,9 @@ string Linker::getLabelName(string token)
 
 void Linker::searchSymbol(string &data, vector<string> tokens)
 {
+    if (tokens[0] == "_start:") //sempre deve ignorar o start (nao deve ser add na tabela de simbolos da secao de texto)
+        return;
+
     uint32_t increment = 4;
     for (size_t i = 0; i < tokens.size(); i++)
     {
@@ -183,21 +261,43 @@ void Linker::searchSymbol(string &data, vector<string> tokens)
                     increment -= 4; //decrementa um espaçamento pois essa variavel ja começa com valor 4
 
                     writeDataSection(data, tokens); //armazena na memoria os dados declarados 'dd'
+                    tokens[i].pop_back();           //tira o dois pontos ':'
+                    dataSymbolsTable[tokens[i]] = dataSymbolAddress;
+                    dataSymbolAddress += increment;
+                    continue;
                 }
             }
-            tokens[i].pop_back();
-            symbolsTable[tokens[i]] = symbolAddress;
-            symbolAddress += increment;
+            //se chegar aqui é pq é um simbolo da secao de texto
+            tokens[i].pop_back(); //tira o dois pontos ':'
+            textSymbolsTable[tokens[i]] = textSymbolAddress;
+            textSymbolAddress += getInstructionIncrementSize(tokens[1]); //instrucao esta na segunda posicao
         }
     }
 }
 
+uint32_t Linker::getInstructionIncrementSize(string instruction)
+{
+    // TODO
+}
+
 bool Linker::isSymbolInTable(string symbol)
 {
-    return !(symbolsTable.find(symbol) == symbolsTable.end());
+    return !(textSymbolsTable.find(symbol) == textSymbolsTable.end()) |
+           !(dataSymbolsTable.find(symbol) == dataSymbolsTable.end());
 }
 
 bool Linker::isInstructionInTable(string instruction)
 {
     return !(instructionsTable.find(instruction) == instructionsTable.end());
+}
+
+bool Linker::isJumpInstruction(string instruction)
+{
+    if (instruction == "JMP" || instruction == "JL" || instruction == "JG" ||
+        instruction == "JE" || instruction == "JNE" || instruction == "JB" ||
+        instruction == "JA" || instruction == "JBE" || instruction == "JGE")
+
+        return true;
+    
+    return false;
 }
--- trab2/ligador_ia32/linker.hpp
+++ trab2/ligador_ia32/linker.hpp
@@ -9,34 +9,6 @@
 
 class Linker
 {
-
-public:
-    Linker()
-    {
-        //TODO - Adicionar variacao das instrucoes
-        instructionsTable["ADD"] = 0x00000000;
-        instructionsTable["SUB"] = 0x00000000;
-        instructionsTable["IMUL"] = 0x00000000;
-        instructionsTable["IDIV"] = 0x00000000;
-        instructionsTable["MOV"] = 0x00000000;
-        instructionsTable["JMP"] = 0x00000000;
-        instructionsTable["CMP"] = 0x00000000;
-        instructionsTable["JL"] = 0x00000000;
-        instructionsTable["JG"] = 0x00000000;
-        instructionsTable["JE"] = 0x00000000;
-        instructionsTable["PUSH"] = 0x00000000;
-        instructionsTable["POP"] = 0x00000000;
-        instructionsTable["INT"] = 0x00000000;
-        instructionsTable["CALL"] = 0x00000000;
-
-        symbolAddress = 0;
-        currentSection = 0;
-
-    };           //construtor
-    ~Linker(){}; //destrutor
-
-    void run(string &text, string &data, string filename);
-
 private:
     void firstPassage(string &data, string filename);
     void secondPassage(string &text, string &data, string filename);
@@ -46,15 +18,74 @@ private:
     void writeBinaryInstructions(string &text, string &data, vector<string> tokens);
     bool setCurrentSection(vector<string> tokens);
 
+    string getInstructionOpcode(vector<string> tokens, int *nextTokenToProcess);
+    void getArgumentsBinaryCode(string &text, string &data, vector<string> tokens, bool isJump);
     int getAddressDisplacement(string token);
     string getLabelName(string token);
     void searchSymbol(string &data, vector<string> tokens);
     bool isSymbolInTable(string symbol);
     bool isInstructionInTable(string instruction);
+    bool isJumpInstruction(string instruction);
+    uint32_t getInstructionIncrementSize(string instruction);
 
-    uint32_t symbolAddress, currentSection;
-    map<string, uint32_t> instructionsTable;
-    map<string, uint32_t> symbolsTable; //string representa o rotulo e inteiro represento o endereço
+    uint32_t textSymbolAddress, currentSection,dataSymbolAddress;
+    map<string, string> instructionsTable;
+    map<string, string> opcodesTable;
+    map<string, uint32_t> dataSymbolsTable;
+    map<string, uint32_t> textSymbolsTable; //string representa o rotulo e inteiro represento o endereço
+
+public:
+    void run(string &text, string &data, string filename);
+
+    Linker()
+    {
+        textSymbolAddress = 0;
+        currentSection = 0;
+        dataSymbolAddress = 0;
+
+        /**
+         * INSTRUCTIONS TABLE
+         * 
+         **/
+        instructionsTable["ADD"] = "0x00000000";
+        instructionsTable["SUB"] = "0x00000000";
+        instructionsTable["IMUL"] = "0x00000000";
+        instructionsTable["IDIV"] = "0x00000000";
+        instructionsTable["MOV"] = "0x00000000";
+        instructionsTable["JMP"] = "0x00000000";
+        instructionsTable["CMP"] = "0x00000000";
+        instructionsTable["JL"] = "0x00000000";
+        instructionsTable["JG"] = "0x00000000";
+        instructionsTable["JE"] = "0x00000000";
+        instructionsTable["PUSH"] = "0x00000000";
+        instructionsTable["POP"] = "0x00000000";
+        instructionsTable["INT"] = "0x00000000";
+
+        //instrucoes das funcoes implementadas em INPUT e OUTPUT
+        instructionsTable["CALL"] = "0x00000000";
+        instructionsTable["RET"] = "0x00000000";
+        instructionsTable["JGE"] = "0x00000000";
+        instructionsTable["INC"] = "0x00000000";
+        instructionsTable["JNE"] = "0x00000000";
+        instructionsTable["MOVZX"] = "0x00000000";
+        instructionsTable["JB"] = "0x00000000";
+        instructionsTable["JBE"] = "0x00000000";
+        instructionsTable["JA"] = "0x00000000";
+        instructionsTable["DEC"] = "0x00000000";
+
+        /**
+         * OPCODES TABLE
+         * 
+         **/
+        opcodesTable["MOV EAX"] = hex2ascii(0xb8, false);
+        opcodesTable["MOV EBX"] = hex2ascii(0xbb, false);
+        opcodesTable["MOV ECX"] = hex2ascii(0xb9, false);
+        opcodesTable["MOV EDX"] = hex2ascii(0xba, false);
+        opcodesTable["INT 0X80"] = hex2ascii(0xcd80, false);
+
+
+    };           //construtor
+    ~Linker(){}; //destrutor
 };
 
 #endif
